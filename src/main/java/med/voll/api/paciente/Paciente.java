a255285e9820154e713de64d0815c4292e0622a8
victojoao <victojoao789@gmail.com>
2023-10-31 13:22:52 -0300

    firt

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,32 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Especialidade;
+
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
